802fa304c29681afb444ee2f1f218067d8654327
David Silin <DSilin17@gmail.com>
2014-11-22 21:19:32 -0600

    more model

--- controllers/debate.js
+++ controllers/debate.js
@@ -1,1 +1,42 @@
 var debate = require('../models/debate');
+
+exports.makeDebate = function(res, req){
+	var debate = new Debate();
+
+	debate.topic = req.body.topic;
+	debate.political = req.body.political;
+	debate.type = req.body.type;
+	debate.topicPre = req.body.topicPre;
+	debate.sidesPre = req.body.sidesPre;
+	debate.openPositions = req.body.openPositions;
+	debate.namespace = "/d" + debate._id;
+	debate.serious = req.body.serious;
+	debate.state = 0;
+	debate.userId = req.user._id;
+	debate.debaters = [debate.user._id];
+	debate.debaterLimit = 2;
+	debate.observers = [];
+
+	debate.save(function(err){
+		if(err)
+			res.send(err);
+
+		res.json({message:"debate created successfully!", namespace: debate.namespace})
+	})
+}
+
+exports.joinDebate = function(res, req){
+	debate.findOne({_id: req.params.debate_id}, function(err, debate){
+		if(err)
+			res.send(err);
+
+		if(debate.debaterLimit > debate.debaters.length){
+			res.json({message:"joined "})
+		}
+	})
+	
+}
+
+exports.getDebates = function(res, req){
+	
+}
--- models/debate.js
+++ models/debate.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 // Define our beer schema
 var DebateSchema   = new mongoose.Schema({
-  name: String, //title of the debate
+  //name: String, //title of the debate
   type: String, //parli, ethical dilemma, presidential
   political: Boolean, //is the debate political?(no:0, yes:1)
   topic: String, //topic of the debate
@@ -12,7 +12,10 @@ var DebateSchema   = new mongoose.Schema({
   namespace: String, //namespace of the debate
   serious: Number, //lower number less serious 1-4
   state: Number, //0 not started, 1 in progress, 2 finished
-  userId: String
+  debaterLimit: Number, //should be determined from the type
+  userId: String, //id of the person who created the debate
+  debaters: Array, //ids of the people debating, including the creator
+  observers: Array, //ids of people observing the debate
 });
 
 // Export the Mongoose model
